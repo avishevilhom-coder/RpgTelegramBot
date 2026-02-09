24277ba3c1954e02f2490e2f7e8015d4a0f2f606
PpoDdd <avishevilhom@gmail.com>
2026-02-09 22:57:38 +0500

    update

--- bot.js
+++ bot.js
@@ -6,14 +6,14 @@
     console.log("Бот работает");
     bot.on('polling_error', console.log);
     const userState = {};
-
+    const players = {}; 
 
     
     bot.on('message', (msg) =>{
         const chatId = msg.chat.id;
         const text = msg.text;
         console.log("Пришло сообщение " , text);
-
+        let player = players[chatId] || {}; 
         
     switch(true){
 case text === '/start':
@@ -41,7 +41,25 @@ case (text === '1' && userState[chatId] === 'vibor_klass' ):
 Мана: 30
 Мана кост: 10
 Урон магией: 15`);  
-            break;
+                    player.className = "Воин";
+                    player.hp= 120;
+                    player.damage= 10;
+                    player.armor= 5;
+                    player.mana= 3;
+                    player.weapon= "Секира";
+                    player.lvl= 1;
+                    player.manacost= 10;
+                    player.magickattack = 15;
+                    player.inventory = [
+                        {name: "Зелье лечение", effect: "Лечение" , type: "Зелье" , value: 20 },
+                        {name: "Секира" ,type: "weapon", damage: 10} 
+                    ];
+
+Stati(msg, text, player); 
+players[chatId] = player;
+break;
+
+
 case (text === '2' && userState[chatId] === 'vibor_klass' ):   
       bot.sendMessage(chatId,`Хороший выбор:
 Лучник имеет свои статы:
@@ -51,7 +69,24 @@ case (text === '2' && userState[chatId] === 'vibor_klass' ):
 Мана: 40
 Мана кост: 10
 Урон магией: 15`);
+                    player.className = "Лучник";
+                    player.hp= 90;
+                    player.damage= 12;
+                    player.armor= 2;
+                    player.mana= 40;
+                    player.weapon= "Лук";
+                    player.lvl= 1;
+                    player.manacost= 10;
+                    player.magickattack = 15;
+                    player.inventory = [
+                        {name: "Зелье лечение", effect: "Лечение" , type: "Зелье" , value: 20 },
+                        {name: "Лук" ,type: "weapon", damage: 12} 
+                    ];
+  Stati(msg, text, player); 
+  players[chatId] = player;                 
   break;
+
+
 case (text === '3' && userState[chatId] === 'vibor_klass' ):  
 bot.sendMessage(chatId,`Хороший выбор:
 Маг имеет свои статы:
@@ -61,11 +96,41 @@ bot.sendMessage(chatId,`Хороший выбор:
 Мана: 50
 Мана кост: 15
 Урон магией: 18`);
+                    player.className = "Маг";
+                    player.hp= 60;
+                    player.damage= 14;
+                    player.armor= 1;
+                    player.mana= 50;
+                    player.weapon= "ЖезлоАлександра 1";
+                    player.lvl= 1;
+                    player.manacost= 15;
+                    player.magickattack = 18;
+                    player.inventory = [
+                        {name: "Зелье лечение", effect: "Лечение" , type: "Зелье" , value: 20 },
+                        {name: "ЖезлоАлександра 1" ,type: "weapon", damage: 14} 
+                    ];
+
+
+Stati(msg, text , player);
+players[chatId] = player;
 break;
 default:
-    console.log("Введите от 1  до 3");
+    bot.sendMessage(chatId, 'Введите от 1 - 3 (нажмите /start');
     break;
         }
 
-        
+function Stati(msg, text, player){
+const inventoryList = player.inventory.map(item => item.name).join (' ,');
+bot.sendMessage(chatId, `Хорошо ты создал ${player.className} Теперь посмотрим его статы
+   Здоровье = ${player.hp}
+   Урон = ${player.damage}
+   Броня = ${player.armor}
+   Мана = ${player.mana}
+   Оружие = ${player.damage}
+   Лвл = ${player.lvl}
+   МанаКост = ${player.manacost}
+   Атака магией = ${player.magickattack}
+   Инвентарь = ${inventoryList}`);
+};
+
 });
